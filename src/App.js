58e1ccc04ef2da20eeb51375a96652e6fcc3c705
mrbluesky1 <emirasanaliev07@gmail.com>
2023-03-01 14:10:47 +0600

    init

--- src/App.js
+++ src/App.js
@@ -1,37 +1,63 @@
-import React from "react";
+import {useForm} from "react-hook-form";
 
-class FormLogin extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            login: ""
-        }
-        this.HandleChange = this.HandleChange.bind(this)
-        this.HandleSubmit = this.HandleSubmit.bind(this)
-    }
+export default function App() {
+    const {register, handleSubmit} = useForm()
+    const onSubmit = data => console.log(data)
 
-    HandleChange(event) {
-        this.setState({value: event.target.value})
-    }
-
-    HandleSubmit(event) {
-        alert("Ваш логин: " + this.state.value)
-        event.preventDefault()
-    }
-
-    render() {
-        return (
-            <div>
-                <form onSubmit={this.HandleSubmit}>
-                    Введите логин:
-                    <label>
-                        <input className='input1' type="text" value={this.state.value} onChange={this.HandleChange}/>
-                    </label>
-                    <input type="submit" value="submit"/>
-                </form>
+    return(
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <p>First name</p>
+            <input {...register("FirstName", {required:true, maxLength: 20})}/>
+            <p>Surname</p>
+            <input {...register("Surname", {pattern: /^[A-Za-z]+$/i})}/>
+            <p>Age</p>
+            <input type={"number"} {...register("Age", {min: 19, max: 99, required:true})}/>
+            <div className={"radios"}>
+                <input className={"radio1"} type="radio" id={"male"} value={"male"} {...register("Gender")}/>
+                <label htmlFor={"male"}>Male</label>
+                <input className={"radio1"} type="radio" id={"female"} value={"female"} {...register("Gender", {required:true})}/>
+                <label htmlFor={"female"}>Female</label>
             </div>
-        );
-    }
+            <input type={"submit"}/>
+        </form>
+    )
 }
 
-export default FormLogin
+
+// import React from "react";
+//
+// class FormLogin extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             login: ""
+//         }
+//         this.HandleChange = this.HandleChange.bind(this)
+//         this.HandleSubmit = this.HandleSubmit.bind(this)
+//     }
+//
+//     HandleChange(event) {
+//         this.setState({value: event.target.value})
+//     }
+//
+//     HandleSubmit(event) {
+//         alert("Ваш логин: " + this.state.value)
+//         event.preventDefault()
+//     }
+//
+//     render() {
+//         return (
+//             <div>
+//                 <form onSubmit={this.HandleSubmit}>
+//                     Введите логин:
+//                     <label>
+//                         <input className='input1' type="text" value={this.state.value} onChange={this.HandleChange}/>
+//                     </label>
+//                     <input type="submit" value="submit"/>
+//                 </form>
+//             </div>
+//         );
+//     }
+// }
+//
+// export default FormLogin
